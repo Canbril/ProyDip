58cf830709daf9136c7df748747cf9c9b6ea6046
Nicolás Jaramillo Arcos <nicolas.jaramilloa@uqvirtual.edu.co>
2024-11-02 12:47:03 -0500

    Se organizó puerto del back

--- client/src/GenerateKeys.js
+++ client/src/GenerateKeys.js
@@ -6,7 +6,7 @@ function GenerateKeys() {
 
     const handleGenerateKeys = async () => {
         try {
-            const response = await fetch('http://localhost:5000/api/generate-keys', {
+            const response = await fetch('http://localhost:4000/api/generate-keys', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ alias }),
