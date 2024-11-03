99cc5cb007317bfdedcf39ae9b2041e2e1c28e0a
Juan Manuel <juanm.adarvec2@uqvirtual.edu.co>
2024-11-02 19:27:07 -0500

    Modificado el CMD del Dockerfile del firewall, para cargar las reglas iptables

---
+++ firewall/start_container.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination $IP_CLIENT:80
+iptables -t nat -A POSTROUTING -p tcp -d $IP_CLIENT --dport 80 -j MASQUERADE
+
+iptables -A OUTPUT -j ACCEPT
+
+while true; do sleep 1; done
