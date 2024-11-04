dfc4ea182636d87b83d1cb7f5eed4c73f79c018d
Juan Manuel <juanm.adarvec2@uqvirtual.edu.co>
2024-11-04 09:59:42 -0500

    Modificadas las variables de entorno en el script de iptables

--- firewall/start_container.sh
+++ firewall/start_container.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
 iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination $IP_CLIENT:80
-iptables -t nat -A POSTROUTING -p tcp -d $IP_CLIENT --dport 80 -j MASQUERADE
+iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination $IP_FRONTEND:80
+iptables -t nat -A POSTROUTING -p tcp -d $IP_FRONTEND --dport 80 -j MASQUERADE
 
-iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-iptables -t nat -A PREROUTING -p tcp --dport 4000 -j DNAT --to-destination $IP_CLIENT:4000
-iptables -t nat -A POSTROUTING -p tcp -d $IP_CLIENT --dport 4000 -j MASQUERADE
+iptables -A INPUT -p tcp --dport 4000 -j ACCEPT
+iptables -t nat -A PREROUTING -p tcp --dport 4000 -j DNAT --to-destination $IP_BACKEND:4000
+iptables -t nat -A POSTROUTING -p tcp -d $IP_BACKEND --dport 4000 -j MASQUERADE
 
 iptables -A OUTPUT -j ACCEPT
 
