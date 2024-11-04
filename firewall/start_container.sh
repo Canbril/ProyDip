9d976030f6cb8255daf98520942e014d8960f8d2
Juan Manuel <juanm.adarvec2@uqvirtual.edu.co>
2024-11-04 09:55:45 -0500

    Configurado el iptables para el puerto del backend

--- firewall/start_container.sh
+++ firewall/start_container.sh
@@ -4,6 +4,10 @@ iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination $IP_CLIENT:80
 iptables -t nat -A POSTROUTING -p tcp -d $IP_CLIENT --dport 80 -j MASQUERADE
 
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -t nat -A PREROUTING -p tcp --dport 4000 -j DNAT --to-destination $IP_CLIENT:4000
+iptables -t nat -A POSTROUTING -p tcp -d $IP_CLIENT --dport 4000 -j MASQUERADE
+
 iptables -A OUTPUT -j ACCEPT
 
 while true; do sleep 1; done
